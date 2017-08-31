a057d4ae82b300081b94ec191c6c213ed2db730b
Matias Jara <mjara.figueroa@gmail.com>
2017-08-31 11:33:56 -0300

    Scope tamaño y sin_usuario en item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+
+  scope :tamaño, ->(x) { where(size: x)}
+  scope :sin_usuario, ->{where(user_id: nil)}
+  
 end
