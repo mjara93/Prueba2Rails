e66f24061d225e4b5bbcf6f93f5545d1abc4b14d
Matias Jara <mjara.figueroa@gmail.com>
2017-08-31 10:58:15 -0300

    generando modelos Category e item, corriendo migracion

---
+++ db/migrate/20170831135534_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.integer :serie
+      t.string :name
+      t.integer :size
+      t.text :description
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170831135534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer  "serie"
+    t.string   "name"
+    t.integer  "size"
+    t.text     "description"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
+  end
+
+  add_foreign_key "items", "categories"
 end
